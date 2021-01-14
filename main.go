a4118e942a93fcc50dcb73a07092407b70941b60
Yohan Alexander <yohanfranca@gmail.com>
2021-01-14 11:21:52 -0300

    test: add test of hello function

--- main.go
+++ main.go
@@ -7,12 +7,17 @@ import (
 	"os"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
+func hello() string {
+	message := "Hello World!"
+	return message
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, hello())
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", greet)
 	log.Printf("Listening on %s...\n", os.Getenv("PORT"))
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		panic(err)
