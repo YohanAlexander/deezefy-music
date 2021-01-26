618188babb1a5f6cbfe0a62991453757b1948e4e
Yohan Alexander <yohanfranca@gmail.com>
2021-01-25 23:32:47 -0300

    feat: entidade ouvinte com telefone multivalorado e testes criados

---
+++ entity/ouvinte/telefone_test.go
@@ -0,0 +1,56 @@
+package ouvinte
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+func TestNewTelefone(t *testing.T) {
+
+	t.Run("Telefone criado com sucesso", func(t *testing.T) {
+		_, err := NewTelefone("vancejoy@gmail.com", "+5579999999999")
+		assert.Nil(t, err)
+	})
+
+}
+
+func TestTelefone_Validate(t *testing.T) {
+
+	type test struct {
+		name     string
+		ouvinte  string
+		telefone string
+		want     error
+	}
+
+	tests := []test{
+		{
+			name:     "Campos válidos",
+			ouvinte:  "vancejoy@gmail.com",
+			telefone: "+5579999999999",
+			want:     nil,
+		},
+		{
+			name:     "Telefone inválido (+5579999999999)",
+			ouvinte:  "vancejoy@gmail.com",
+			telefone: "79999999999",
+			want:     entity.ErrInvalidEntity,
+		},
+		{
+			name:     "Email inválido (vance@gmail.com)",
+			ouvinte:  "",
+			telefone: "+5579999999999",
+			want:     entity.ErrInvalidEntity,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewTelefone(tc.ouvinte, tc.telefone)
+			assert.Equal(t, err, tc.want)
+		})
+	}
+
+}
