e31ca1a9f5218a51beeb0190ec205e2a923260d3
Yohan Alexander <yohanfranca@gmail.com>
2021-01-26 18:26:30 -0300

    feat: entidade musica e testes criados

---
+++ entity/musica/musica.go
@@ -0,0 +1,36 @@
+package musica
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// Musica entidade Musica
+type Musica struct {
+	ID      int    `validate:"required,gte=1"`
+	Duracao int    `validate:"required,gte=100"`
+	Nome    string `validate:"required,gte=1"`
+}
+
+// NewMusica cria um novo Musica
+func NewMusica(id, duracao int, nome string) (*Musica, error) {
+	m := &Musica{
+		ID:      id,
+		Duracao: duracao,
+		Nome:    nome,
+	}
+	err := m.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// Validate valida os dados do Musica
+func (m *Musica) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(m); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
