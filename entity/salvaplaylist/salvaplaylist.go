f283e773685567d3903c32b4347986ba7e60cd84
Yohan Alexander <yohanfranca@gmail.com>
2021-01-27 01:40:09 -0300

    feat: relacionamento salvaplaylist e testes criados

---
+++ entity/salvaplaylist/salvaplaylist.go
@@ -0,0 +1,34 @@
+package salvaplaylist
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// SalvaPlaylist entidade SalvaPlaylist
+type SalvaPlaylist struct {
+	Playlist string `validate:"required,gte=1"`
+	Ouvinte  string `validate:"required,email"`
+}
+
+// NewSalvaPlaylist cria um novo SalvaPlaylist
+func NewSalvaPlaylist(playlist, ouvinte string) (*SalvaPlaylist, error) {
+	sp := &SalvaPlaylist{
+		Playlist: playlist,
+		Ouvinte:  ouvinte,
+	}
+	err := sp.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return sp, nil
+}
+
+// Validate valida os dados do SalvaPlaylist
+func (sp *SalvaPlaylist) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(sp); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
---
+++ entity/salvaplaylist/salvaplaylist_test.go
@@ -0,0 +1,57 @@
+package salvaplaylist
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+func TestNewSalvaPlaylist(t *testing.T) {
+
+	t.Run("SalvaPlaylist criada com sucesso", func(t *testing.T) {
+		sp, err := NewSalvaPlaylist("Indie Rock", "ouvinte@email.com")
+		assert.Nil(t, err)
+		assert.Equal(t, sp.Playlist, "Indie Rock")
+	})
+
+}
+
+func TestSalvaPlaylist_Validate(t *testing.T) {
+
+	type test struct {
+		name     string
+		playlist string
+		ouvinte  string
+		want     error
+	}
+
+	tests := []test{
+		{
+			name:     "Campos válidos",
+			playlist: "Indie Rock",
+			ouvinte:  "ouvinte@email.com",
+			want:     nil,
+		},
+		{
+			name:     "Playlist inválida",
+			playlist: "",
+			ouvinte:  "ouvinte@email.com",
+			want:     entity.ErrInvalidEntity,
+		},
+		{
+			name:     "Ouvinte inválido",
+			playlist: "Indie Rock",
+			ouvinte:  "",
+			want:     entity.ErrInvalidEntity,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewSalvaPlaylist(tc.playlist, tc.ouvinte)
+			assert.Equal(t, err, tc.want)
+		})
+	}
+
+}
