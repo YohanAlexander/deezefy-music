76a2f9f5c9e79f80f110511f8dfb553c210a538c
Yohan Alexander <yohanfranca@gmail.com>
2021-01-27 01:57:21 -0300

    feat: relacionamento artistagenero e testes criados

---
+++ entity/artistagenero/artistagenero.go
@@ -0,0 +1,34 @@
+package artistagenero
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// ArtistaGenero entidade ArtistaGenero
+type ArtistaGenero struct {
+	Artista string `validate:"required,email"`
+	Genero  string `validate:"required,gte=1"`
+}
+
+// NewArtistaGenero cria um novo ArtistaGenero
+func NewArtistaGenero(artista, genero string) (*ArtistaGenero, error) {
+	ag := &ArtistaGenero{
+		Artista: artista,
+		Genero:  genero,
+	}
+	err := ag.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return ag, nil
+}
+
+// Validate valida os dados do ArtistaGenero
+func (ag *ArtistaGenero) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(ag); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
