5e9a64d98cae673de9b980c5364bf2e07bf1be6d
Yohan Alexander <yohanfranca@gmail.com>
2021-01-26 23:15:24 -0300

    feat: entidade genero e testes criados

---
+++ entity/genero/genero_test.go
@@ -0,0 +1,57 @@
+package genero
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+func TestNewGenero(t *testing.T) {
+
+	t.Run("Genero criado com sucesso", func(t *testing.T) {
+		g, err := NewGenero("Indie Rock", "rock")
+		assert.Nil(t, err)
+		assert.Equal(t, g.Estilo, "rock")
+	})
+
+}
+
+func TestGenero_Validate(t *testing.T) {
+
+	type test struct {
+		name   string
+		nome   string
+		estilo string
+		want   error
+	}
+
+	tests := []test{
+		{
+			name:   "Campos válidos",
+			nome:   "Indie Rock",
+			estilo: "rock",
+			want:   nil,
+		},
+		{
+			name:   "Nome inválido",
+			nome:   "",
+			estilo: "rock",
+			want:   entity.ErrInvalidEntity,
+		},
+		{
+			name:   "Estilo inválido",
+			nome:   "Indie Rock",
+			estilo: "indie",
+			want:   entity.ErrInvalidEntity,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewGenero(tc.nome, tc.estilo)
+			assert.Equal(t, err, tc.want)
+		})
+	}
+
+}
