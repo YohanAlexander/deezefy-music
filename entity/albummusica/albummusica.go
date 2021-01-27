17cf4e39ea173ff356939c03ee2fe5ab23040ac9
Yohan Alexander <yohanfranca@gmail.com>
2021-01-27 02:27:54 -0300

    feat: relacionamento albummusica e testes criados

---
+++ entity/albummusica/albummusica.go
@@ -0,0 +1,36 @@
+package albummusica
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// AlbumMusica entidade AlbumMusica
+type AlbumMusica struct {
+	Album   int    `validate:"required,gte=1"`
+	Artista string `validate:"required,email"`
+	Musica  int    `validate:"required,gte=1"`
+}
+
+// NewAlbumMusica cria um novo AlbumMusica
+func NewAlbumMusica(artista string, album, musica int) (*AlbumMusica, error) {
+	am := &AlbumMusica{
+		Album:   album,
+		Artista: artista,
+		Musica:  musica,
+	}
+	err := am.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return am, nil
+}
+
+// Validate valida os dados do AlbumMusica
+func (am *AlbumMusica) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(am); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
