c1b81919c80f3e231076648e82d26ed0c14936f3
Yohan Alexander <yohanfranca@gmail.com>
2021-01-27 01:10:22 -0300

    feat: relacionamento segue e testes criados

---
+++ entity/segue/segue.go
@@ -0,0 +1,34 @@
+package segue
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// Segue entidade Segue
+type Segue struct {
+	Artista string `validate:"required,email"`
+	Ouvinte string `validate:"required,email"`
+}
+
+// NewSegue cria um novo Segue
+func NewSegue(artista, ouvinte string) (*Segue, error) {
+	s := &Segue{
+		Artista: artista,
+		Ouvinte: ouvinte,
+	}
+	err := s.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+// Validate valida os dados do Segue
+func (s *Segue) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(s); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
