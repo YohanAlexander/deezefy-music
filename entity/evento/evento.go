762d5656762b44f0a0021a775ec0907d028d68f9
Yohan Alexander <yohanfranca@gmail.com>
2021-01-26 23:58:45 -0300

    feat: entidade evento e testes criados

---
+++ entity/evento/evento.go
@@ -0,0 +1,36 @@
+package evento
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// Evento entidade Evento
+type Evento struct {
+	Usuario string `validate:"required,email"`
+	Nome    string `validate:"required,gte=1"`
+	ID      int    `validate:"required,gte=1"`
+}
+
+// NewEvento cria um novo Evento
+func NewEvento(usuario, nome string, id int) (*Evento, error) {
+	e := &Evento{
+		Usuario: usuario,
+		Nome:    nome,
+		ID:      id,
+	}
+	err := e.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return e, nil
+}
+
+// Validate valida os dados do Evento
+func (e *Evento) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(e); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
