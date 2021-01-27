18245fe5d6ada3e47679e71bd870128c7731a16a
Yohan Alexander <yohanfranca@gmail.com>
2021-01-27 00:55:19 -0300

    feat: entidade perfil e testes criados

---
+++ entity/perfil/perfil.go
@@ -0,0 +1,36 @@
+package perfil
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/yohanalexander/deezefy-music/entity"
+)
+
+// Perfil entidade Perfil
+type Perfil struct {
+	Ouvinte               string `validate:"required,email"`
+	ID                    int    `validate:"required,gte=1"`
+	InformacoesRelevantes string `validate:"required,gte=1"`
+}
+
+// NewPerfil cria um novo Perfil
+func NewPerfil(ouvinte, informacoesrelevantes string, id int) (*Perfil, error) {
+	p := &Perfil{
+		ID:                    id,
+		Ouvinte:               ouvinte,
+		InformacoesRelevantes: informacoesrelevantes,
+	}
+	err := p.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+// Validate valida os dados do Perfil
+func (p *Perfil) Validate() error {
+	vld := validator.New()
+	if err := vld.Struct(p); err != nil {
+		return entity.ErrInvalidEntity
+	}
+	return nil
+}
