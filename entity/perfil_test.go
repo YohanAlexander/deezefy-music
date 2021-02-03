12de4e94a56ad59af63248f1222a18388088dd0b
Yohan Alexander <yohanfranca@gmail.com>
2021-02-03 02:24:15 -0300

    feat: add relacionamento N:N de favoritos para perfil e artista

--- entity/perfil_test.go
+++ entity/perfil_test.go
@@ -84,3 +84,64 @@ func TestPerfil_Validate(t *testing.T) {
 	}
 
 }
+
+func TestAddArtistasFavoritos(t *testing.T) {
+
+	t.Run("Artista criado com sucesso", func(t *testing.T) {
+		p, _ := NewPerfil("syml@spotify.com", "somepassword", "2018-02-10", "Vance", "Joy", "Where is my love", 1)
+		a, _ := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		err := p.AddArtista(*a)
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(p.ArtistasFavoritos))
+	})
+
+	t.Run("Artista já registrado", func(t *testing.T) {
+		p, _ := NewPerfil("syml@spotify.com", "somepassword", "2018-02-10", "Vance", "Joy", "Where is my love", 1)
+		a, _ := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		err := p.AddArtista(*a)
+		assert.Nil(t, err)
+		a, _ = NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		err = p.AddArtista(*a)
+		assert.Equal(t, ErrArtistaRegistered, err)
+	})
+
+}
+
+func TestRemoveArtistasFavoritos(t *testing.T) {
+
+	t.Run("Artista não cadastrado", func(t *testing.T) {
+		p, _ := NewPerfil("syml@spotify.com", "somepassword", "2018-02-10", "Vance", "Joy", "Where is my love", 1)
+		a, _ := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		err := p.RemoveArtista(*a)
+		assert.Equal(t, ErrNotFound, err)
+	})
+
+	t.Run("Artista removido com sucesso", func(t *testing.T) {
+		p, _ := NewPerfil("syml@spotify.com", "somepassword", "2018-02-10", "Vance", "Joy", "Where is my love", 1)
+		a, _ := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		_ = p.AddArtista(*a)
+		err := p.RemoveArtista(*a)
+		assert.Nil(t, err)
+	})
+
+}
+
+func TestGetArtistasFavoritos(t *testing.T) {
+
+	t.Run("Artista cadastrado encontrado", func(t *testing.T) {
+		p, _ := NewPerfil("syml@spotify.com", "somepassword", "2018-02-10", "Vance", "Joy", "Where is my love", 1)
+		a, _ := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		_ = p.AddArtista(*a)
+		artista, err := p.GetArtista(*a)
+		assert.Nil(t, err)
+		assert.Equal(t, artista, *a)
+	})
+
+	t.Run("Artista não cadastrado", func(t *testing.T) {
+		p, _ := NewPerfil("syml@spotify.com", "somepassword", "2018-02-10", "Vance", "Joy", "Where is my love", 1)
+		a, _ := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
+		_, err := p.GetArtista(*a)
+		assert.Equal(t, ErrNotFound, err)
+	})
+
+}
