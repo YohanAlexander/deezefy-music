2361defbaf01f0f1a01e2be5ed60cfdadff971fd
Yohan Alexander <yohanfranca@gmail.com>
2021-02-02 23:31:40 -0300

    feat: add relacionamento 1:1 de usuario em artista

--- entity/artista_test.go
+++ entity/artista_test.go
@@ -9,7 +9,7 @@ import (
 func TestNewArtista(t *testing.T) {
 
 	t.Run("Artista criado com sucesso", func(t *testing.T) {
-		a, err := NewArtista("vancejoy@gmail.com", "Vance Joy", "Australian Singer", 2006)
+		a, err := NewArtista("vancejoy@gmail.com", "somepassword", "2018-02-10", "Vance Joy", "Australian Singer", 2006)
 		assert.Nil(t, err)
 		assert.Equal(t, a.NomeArtistico, "Vance Joy")
 	})
@@ -18,9 +18,15 @@ func TestNewArtista(t *testing.T) {
 
 func TestArtista_Validate(t *testing.T) {
 
+	type user struct {
+		email    string
+		password string
+		birthday string
+	}
+
 	type test struct {
 		name          string
-		usuario       string
+		usuario       user
 		nomeartistico string
 		biografia     string
 		anoformacao   int
@@ -29,40 +35,48 @@ func TestArtista_Validate(t *testing.T) {
 
 	tests := []test{
 		{
-			name:          "Campos válidos",
-			usuario:       "vancejoy@gmail.com",
+			name: "Campos válidos",
+			usuario: user{
+				email:    "vancejoy@gmail.com",
+				password: "new_password",
+				birthday: "2006-01-02",
+			},
 			nomeartistico: "Vance Joy",
 			biografia:     "Australian Singer",
 			anoformacao:   2006,
 			want:          nil,
 		},
 		{
-			name:          "NomeArtistico inválido",
-			usuario:       "vancejoy@gmail.com",
+			name: "NomeArtistico inválido",
+			usuario: user{
+				email:    "vancejoy@gmail.com",
+				password: "new_password",
+				birthday: "2006-01-02",
+			},
 			nomeartistico: "",
 			biografia:     "Australian Singer",
 			anoformacao:   2006,
 			want:          ErrInvalidEntity,
 		},
 		{
-			name:          "Biografia inválida",
-			usuario:       "vancejoy@gmail.com",
+			name: "Biografia inválida",
+			usuario: user{
+				email:    "vancejoy@gmail.com",
+				password: "new_password",
+				birthday: "2006-01-02",
+			},
 			nomeartistico: "Vance Joy",
 			biografia:     "",
 			anoformacao:   2006,
 			want:          ErrInvalidEntity,
 		},
 		{
-			name:          "Email inválido (vance@gmail.com)",
-			usuario:       "",
-			nomeartistico: "Vance Joy",
-			biografia:     "Australian Singer",
-			anoformacao:   2006,
-			want:          ErrInvalidEntity,
-		},
-		{
-			name:          "AnoFormacao inválido (2000)",
-			usuario:       "",
+			name: "AnoFormacao inválido (2000)",
+			usuario: user{
+				email:    "vancejoy@gmail.com",
+				password: "new_password",
+				birthday: "2006-01-02",
+			},
 			nomeartistico: "Vance Joy",
 			biografia:     "Australian Singer",
 			anoformacao:   98,
@@ -72,7 +86,7 @@ func TestArtista_Validate(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			_, err := NewArtista(tc.usuario, tc.nomeartistico, tc.biografia, tc.anoformacao)
+			_, err := NewArtista(tc.usuario.email, tc.usuario.password, tc.usuario.birthday, tc.nomeartistico, tc.biografia, tc.anoformacao)
 			assert.Equal(t, err, tc.want)
 		})
 	}
