0f90e907095573dd8f0ee06504a8d0721a0cd0d3
Yohan Alexander <yohanfranca@gmail.com>
2021-02-03 06:40:43 -0300

    refactor: update service do caso de uso album

--- usecase/album/interface.go
+++ usecase/album/interface.go
@@ -2,11 +2,15 @@ package album
 
 import "github.com/yohanalexander/deezefy-music/entity"
 
-// Album interface
-type Album interface {
+// Read interface
+type Read interface {
 	Get(id int) (*entity.Album, error)
 	Search(query string) ([]*entity.Album, error)
 	List() ([]*entity.Album, error)
+}
+
+// Write interface
+type Write interface {
 	Create(e *entity.Album) (int, error)
 	Update(e *entity.Album) error
 	Delete(id int) error
@@ -14,7 +18,8 @@ type Album interface {
 
 // Repository interface
 type Repository interface {
-	Album
+	Read
+	Write
 }
 
 // UseCase interface
@@ -22,7 +27,7 @@ type UseCase interface {
 	GetAlbum(id int) (*entity.Album, error)
 	SearchAlbums(query string) ([]*entity.Album, error)
 	ListAlbums() ([]*entity.Album, error)
-	CreateAlbum(id, anolancamento int, titulo, artista string) (int, error)
+	CreateAlbum(email, password, birthday, nomeartistico, biografia, titulo string, anoformacao, anolancamento, id int) (int, error)
 	UpdateAlbum(e *entity.Album) error
 	DeleteAlbum(id int) error
 }
