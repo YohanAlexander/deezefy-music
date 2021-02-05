96d3145563f1481a7ffaeff20dd858559a4ab8b3
Yohan Alexander <yohanfranca@gmail.com>
2021-02-05 00:16:35 -0300

    feat: adicionada função para calcular a idade na camada de presenters

--- api/presenter/artista.go
+++ api/presenter/artista.go
@@ -1,5 +1,9 @@
 package presenter
 
+import (
+	"time"
+)
+
 // Artista entidade Artista
 type Artista struct {
 	Usuario       Usuario   `json:"usuario"`
@@ -11,4 +15,25 @@ type Artista struct {
 	Perfis        []Perfil  `json:"perfis"`
 	Generos       []Genero  `json:"generos"`
 	Albums        []Album   `json:"albums"`
+	Idade         int       `json:"idade"`
+}
+
+// GetIdade calcula a idade do usuario
+func (a *Artista) GetIdade(now time.Time) {
+
+	birthDate, err := parseBirthday(a.Usuario.Birthday)
+
+	if err != nil {
+		a.Idade = 0
+	}
+
+	age := now.Year() - birthDate.Year()
+
+	birthDay := getAdjustedBirthDay(birthDate, now)
+	if now.YearDay() < birthDay {
+		age--
+	}
+
+	a.Idade = age
+
 }
--- api/presenter/ouvinte.go
+++ api/presenter/ouvinte.go
@@ -1,5 +1,9 @@
 package presenter
 
+import (
+	"time"
+)
+
 // Ouvinte entidade Ouvinte
 type Ouvinte struct {
 	Usuario      Usuario    `json:"usuario"`
@@ -10,4 +14,25 @@ type Ouvinte struct {
 	Curtidas     []Musica   `json:"curtidas"`
 	Playlists    []Playlist `json:"playlists"`
 	Albums       []Album    `json:"albums"`
+	Idade        int        `json:"idade"`
+}
+
+// GetIdade calcula a idade do usuario
+func (o *Ouvinte) GetIdade(now time.Time) {
+
+	birthDate, err := parseBirthday(o.Usuario.Birthday)
+
+	if err != nil {
+		o.Idade = 0
+	}
+
+	age := now.Year() - birthDate.Year()
+
+	birthDay := getAdjustedBirthDay(birthDate, now)
+	if now.YearDay() < birthDay {
+		age--
+	}
+
+	o.Idade = age
+
 }
