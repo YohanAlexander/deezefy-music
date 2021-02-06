ab10fff4d406a282f19fe11751dca1088b80a13b
Yohan Alexander <yohanfranca@gmail.com>
2021-02-05 21:16:20 -0300

    feat: camada externa com handler http de perfil

---
+++ api/handler/entity/perfil/perfil.go
@@ -0,0 +1,203 @@
+package perfil
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/yohanalexander/deezefy-music/usecase/entity/perfil"
+
+	"github.com/yohanalexander/deezefy-music/api/presenter"
+
+	"github.com/yohanalexander/deezefy-music/entity"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func listPerfils(service perfil.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+
+		var data []*entity.Perfil
+		var err error
+		id := r.URL.Query().Get("id")
+
+		switch {
+		case id == "":
+			data, err = service.ListPerfils()
+		default:
+			data, err = service.SearchPerfils(id)
+		}
+
+		if err != nil && err != entity.ErrNotFound {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrUnexpected.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+
+		if data == nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrNotFound.Error(),
+				StatusCode: http.StatusNotFound,
+			})
+			return
+		}
+
+		var toJ []*presenter.Perfil
+		for _, d := range data {
+			toJ = presenter.AppendPerfil(*d, toJ)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(toJ); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrJSON.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+	})
+}
+
+func createPerfil(service perfil.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+
+		input := &presenter.Perfil{}
+		err := json.NewDecoder(r.Body).Decode(&input)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrJSON.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+
+		music, err := service.CreatePerfil(input.Ouvinte.Usuario.Email, input.Ouvinte.Usuario.Password, input.Ouvinte.Usuario.Birthday, input.Ouvinte.PrimeiroNome, input.Ouvinte.Sobrenome, input.InformacoesRelevantes, input.ID)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrUnexpected.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+
+		m, err := service.GetPerfil(music)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrUnexpected.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+
+		toJ := &presenter.Perfil{}
+		toJ.GetPerfil(*m)
+
+		w.WriteHeader(http.StatusCreated)
+		if err := json.NewEncoder(w).Encode(toJ); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrJSON.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+	})
+}
+
+func getPerfil(service perfil.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+
+		vars := mux.Vars(r)
+		idstr := vars["id"]
+		id, _ := strconv.Atoi(idstr)
+
+		data, err := service.GetPerfil(id)
+		if err != nil && err != entity.ErrNotFound {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrUnexpected.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+
+		if data == nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrNotFound.Error(),
+				StatusCode: http.StatusNotFound,
+			})
+			return
+		}
+
+		toJ := &presenter.Perfil{}
+		toJ.GetPerfil(*data)
+
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(toJ); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrJSON.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+	})
+}
+
+func deletePerfil(service perfil.UseCase) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+
+		vars := mux.Vars(r)
+		idstr := vars["id"]
+		id, _ := strconv.Atoi(idstr)
+
+		err := service.DeletePerfil(id)
+
+		w.WriteHeader(http.StatusOK)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(presenter.Erro{
+				Message:    presenter.ErrUnexpected.Error(),
+				StatusCode: http.StatusInternalServerError,
+			})
+			return
+		}
+	})
+}
+
+//MakePerfilHandlers make url handlers
+func MakePerfilHandlers(r *mux.Router, n negroni.Negroni, service perfil.UseCase) {
+	r.Handle("/v1/perfil", n.With(
+		negroni.Wrap(listPerfils(service)),
+	)).Methods("GET", "OPTIONS").Name("listPerfils")
+
+	r.Handle("/v1/perfil", n.With(
+		negroni.Wrap(createPerfil(service)),
+	)).Methods("POST", "OPTIONS").Name("createPerfil")
+
+	r.Handle("/v1/perfil/{id}", n.With(
+		negroni.Wrap(getPerfil(service)),
+	)).Methods("GET", "OPTIONS").Name("getPerfil")
+
+	r.Handle("/v1/perfil/{id}", n.With(
+		negroni.Wrap(deletePerfil(service)),
+	)).Methods("DELETE", "OPTIONS").Name("deletePerfil")
+}
